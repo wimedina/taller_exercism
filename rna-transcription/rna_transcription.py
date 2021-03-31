5b9babbb397b8e4c43caab02fe71a1a70b104b2c
wimedina <wimedina93@gmail.com>
2021-03-30 23:02:42 -0500

    Ejercicio rna-transcription terminado y subido a exercism

---
+++ rna-transcription/rna_transcription.py
@@ -0,0 +1,15 @@
+def to_rna(dna_strand):
+    rna_strand = ""
+    if dna_strand == "":
+        return rna_strand
+    else:
+        for i in range(0, len(dna_strand)):
+            if dna_strand[i] == "C":
+                rna_strand += "G"
+            elif dna_strand[i] == "G":
+                rna_strand += "C"
+            elif dna_strand[i] == "T":
+                rna_strand += "A"
+            elif dna_strand[i] == "A":
+                rna_strand += "U"            
+    return rna_strand
